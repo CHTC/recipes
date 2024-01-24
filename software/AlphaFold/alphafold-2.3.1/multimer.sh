8dbee41a7ba1ce90692e082253fe3227f0dc29f6
Christina K <christinakconnect@gmail.com>
2024-01-23 18:55:06 -0600

    Create multimer.sh

---
+++ software/AlphaFold/alphafold-2.3.1/multimer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+FASTA=$1
+MAX_DATE=$2
+NUM_MPPM=$3
+OUTDIR=outputs
+mkdir $OUTDIR
+
+DATADIR=/gpulab_data/alphafold_2023_06_09
+
+source /opt/miniconda3/etc/profile.d/conda.sh
+conda activate alphafold
+python3 /opt/alphafold/run_alphafold.py \
+  --fasta_paths=${FASTA} \
+  --data_dir=${DATADIR} \
+  --output_dir=${OUTDIR} \
+  --max_template_date=${MAX_DATE} \
+  --bfd_database_path=${DATADIR}/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt \
+  --mgnify_database_path=${DATADIR}/mgnify/mgy_clusters_2022_05.fa \
+  --template_mmcif_dir=${DATADIR}/pdb_mmcif/mmcif_files \
+  --obsolete_pdbs_path=${DATADIR}/pdb_mmcif/obsolete.dat \
+  --uniref90_database_path=${DATADIR}/uniref90/uniref90.fasta \
+  --uniref30_database_path=${DATADIR}/uniref30/UniRef30_2021_03 \
+  --pdb_seqres_database_path=${DATADIR}/pdb_seqres/pdb_seqres.txt \
+  --uniprot_database_path=${DATADIR}/uniprot/uniprot.fasta \
+  --num_multimer_predictions_per_model=${NUM_MPPM} \
+  --model_preset', 'multimer' \
+  --use_gpu_relax=true
